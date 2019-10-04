2eafd23b4fecd7762a5d317de1e88b451be10720
Jhonatancs <jdc_santos@outlook.com>
2019-10-04 19:36:24 -0300

    adicionando arquivo para listar usuarios

---
+++ listar.php
@@ -0,0 +1,24 @@
+<?php 
+	header("Access-Control-Allow-Origin: *");
+	if(isset($_POST['listar'])){
+
+		require_once 'Connect.php';
+
+		$query = "SELECT * FROM usuario ORDER BY nm_usuario";
+		$sql = $conn->prepare($query);
+		$sql->execute();
+		$res = $sql->fetch();
+		
+		if(is_array($res)){
+			$retorno['status'] = 1;
+			$retorno['usuarios'] = $res;
+			echo json_encode($retorno);
+		}else{
+			$retorno['status'] = 0;
+			$retorno['usuarios'] = null;
+			echo json_encode($retorno);
+		}
+	}else{
+		echo "requisição sem parametros!";
+	}
+	
