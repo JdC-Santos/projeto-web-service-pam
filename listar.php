93e9dfd6575813a513a24ca3a55d128a03a90db2
Jhonatancs <jdc_santos@outlook.com>
2019-10-04 20:01:07 -0300

    adicionando arquivo para listar usuarios

--- listar.php
+++ listar.php
@@ -1,34 +1,27 @@
 <?php 
 	header("Access-Control-Allow-Origin: *");
-	if(isset($_GET['listar'])){
+	if(isset($_POST['listar'])){
 
 		require_once 'Connect.php';
 
-		$query = "SELECT * FROM usuario ORDER BY nm_usuario";
+		$query = "SELECT cd_usuario, nm_usuario, email_usuario FROM usuario ";
 		$sql = $conn->prepare($query);
 		$sql->execute();
-		$res = $sql->fetch();
 		
-		if(is_array($res)){
-			$usuarios = [];
-			$i = 0;
-			print_r($res);
-			foreach($res as $key => $value){
-			
-				$usuarios[$i]['cd'] = $value['cd_usuario'];
-				$usuarios[$i]['nome'] = $value['nm_usuario'];
-				$usuarios[$i]['email'] = $value['email_usuario'];
-				$i++;
-			}
-
-			$retorno['status'] = 1;
-			$retorno['usuarios'] = $usuarios;
-			echo json_encode($retorno);
-		}else{
-			$retorno['status'] = 0;
-			$retorno['usuarios'] = null;
-			echo json_encode($retorno);
+		$usuarios = [];
+		$i = 0;
+		
+		while($fetch = $sql->fetch(PDO::FETCH_ASSOC)){
+			$usuarios[$i]['cd'] = $fetch['cd_usuario'];
+			$usuarios[$i]['nome'] = $fetch['nm_usuario'];
+			$usuarios[$i]['email'] = $fetch['email_usuario'];
+			$i++;
 		}
+		
+		$retorno['status'] = 1;
+		$retorno['usuarios'] = $usuarios;
+		echo json_encode($retorno);
+
 	}else{
 		echo "requisição sem parametros!";
 	}
