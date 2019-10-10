e3835211d2d46922497002431cff01bd7a282ae7
Jhonatancs <jdc_santos@outlook.com>
2019-10-09 22:48:15 -0300

    tirando ponto de debug

--- excluir.php
+++ excluir.php
@@ -5,7 +5,6 @@
 		require_once 'Connect.php';
 
 		$query = "DELETE FROM usuario WHERE cd_usuario = ".$_POST['excluir']." LIMIT 1";
-		echo $query;
 		$sql = $conn->prepare($query);
 		if($sql->execute()){
 			$retorno['status'] = 1;
