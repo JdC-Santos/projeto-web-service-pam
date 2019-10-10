0196f41e661000d85d1db36096892bdd55eb57f1
Jhonatancs <jdc_santos@outlook.com>
2019-10-09 21:49:53 -0300

    adicionando metodo para excluir usuarios

---
+++ excluir.php
@@ -0,0 +1,22 @@
+<?php 
+	header("Access-Control-Allow-Origin: *");
+	if(isset($_POST['excuir'])){
+
+		require_once 'Connect.php';
+
+		$query = "DELETE FROM usuario WHERE cd_usuario = ".$_POST['excuir']." LIMIT 1";
+		$sql = $conn->prepare($query);
+		if($sql->execute()){
+			$retorno['status'] = 1;
+			$retorno['msg'] = "Usuário deletado com sucesso!";
+			echo json_encode($retorno);
+		}else{
+			$retorno['status'] = 0;
+			$retorno['msg'] = "Erro: não foi possivel excluir o usuário!";
+			echo json_encode($retorno);
+		}
+	
+	}else{
+		echo "requisição sem parametros!";
+	}
+	
